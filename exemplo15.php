630661925f70123453ec8d65e79c6d577c757287
analatorre <ana.b.silva264@aluno.senai.br>
2026-04-15 09:02:51 -0300

    sobrescrevi o metodo creditar
    muda meu saldo e atualiza o meu juros, contabilizando mais

---
+++ exemplo15.php
@@ -0,0 +1,71 @@
+<?php
+
+class Conta
+{
+    // Atributos - Propriedades - Campls
+    public $numero;
+    public $saldo;
+
+    // Método - Construtor
+    function __construct($numero, $saldo)
+    {
+        // this vai pegar la da class conta
+        $this->numero = $numero;
+        $this->saldo = $saldo;
+    }
+
+    // Métodos
+    function creditar($valor)
+    {
+        $this->saldo = $this->saldo + $valor;
+    }
+
+    function debitar($valor)
+    {
+        $this->saldo = $this->saldo - $valor;
+    }
+
+    function transferir($outraConta, $valor)
+    {
+        if ($this->saldo >= $valor) {
+            $this->debitar($valor);
+            $outraConta->creditar($valor);
+        }
+    }
+}
+
+class Poupanca extends Conta
+{
+    public $juros;
+
+    function __construct($numero, $saldo, $juros) //inicializar o juros para passar um valor de juros para um cliente específico
+    {
+        parent::__construct($numero, $saldo); //o filho classe poupança chama o construtor da classe pai (Conta) para inicializar os atributos numero e saldo
+        $this->juros = $juros;
+    }
+
+    function creditar($valor)
+    {
+        parent::creditar($valor); //chama o método creditar da classe pai (Conta) para adicionar o valor ao saldo
+        $this->atualizarJuros(); //atualiza os juros após o crédito
+    }
+
+    function atualizarJuros()
+    {
+        $this->saldo = $this->saldo * (1 + $this->juros);
+    }
+}
+
+// Criando um objeto da classe Conta
+$conta = new Conta(1, 150);
+$conta->creditar(50);
+$conta->debitar(100);
+echo "Saldo da conta: $conta->numero = R$$conta->saldo,00 <br>";
+
+
+// Criando um objeto da classe Poupanca
+$poupanca = new Poupanca(2, 150, 0.10); // 2 - numero; 150 - saldo; 0.10 - juros;
+$poupanca->creditar(50);
+$poupanca->debitar(100);
+$poupanca->atualizarJuros();
+echo "Saldo da poupança: $poupanca->numero = R$$poupanca->saldo,00 <br>";
