b3c76662729fd4b5c128f8a96ae03f76115fbb35
analatorre <ana.b.silva264@aluno.senai.br>
2026-04-29 16:40:32 -0300

    finalizado 20

--- exemplo20.php
+++ exemplo20.php
@@ -1,5 +1,6 @@
 <?php
 
+// interface é um contrato que criamos, onde definimos os métodos que as classes que implementarem essa interface devem ter.
 interface INotificador {
     public function enviar($destinatario, $mensagem);
 }
@@ -27,3 +28,26 @@ class NotificadorWhatsapp implements INotificador {
         echo "Whatsapp enviado para {destinatario}. Mensagem: {$mensagem}.";
     }
 }
+
+// Classe que usa a interface
+class SistemaDeNotificacoes {
+    private $notificador;
+
+    public function __construct(INotificador $notificador)
+    {
+        $this->notificador = $notificador;
+    }
+
+    public function notificarUsuario($destinatario, $mensagem)
+    {
+        $this->notificador->enviar($destinatario, $mensagem);
+    }
+}
+
+$sistemaEmail = new SistemaDeNotificacoes(new NotificadorEmail());
+$sistemaSMS = new SistemaDeNotificacoes(new NotificadorSMS());
+$sistemaWhatsapp = new SistemaDeNotificacoes(new NotificadorWhatsapp());
+
+$sistemaEmail->notificarUsuario("joao@email.com", "Seu Pedido foi confirmado");
+$sistemaSMS->notificarUsuario("17997651234", "Seu Pedido foi confirmado");
+$sistemaWhatsapp->notificarUsuario("17997651234", "Seu Pedido foi confirmado");
